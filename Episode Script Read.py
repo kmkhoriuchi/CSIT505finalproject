7b6cacd1575faddefe1841a9067daaeb3365ffad
kmkhoriuchi <60794263+kmkhoriuchi@users.noreply.github.com>
2021-06-19 14:39:36 -0400

    Update Episode Script Read.py

--- Episode Script Read.py
+++ Episode Script Read.py
@@ -6,8 +6,6 @@ Created on Mon Jun 14 16:15:55 2021
 @author: kirahoriuchi
 """
 
-import string
-
 in_file1 = open("Episode_IV.txt", "r")
 out_file1 = open("Episode_IV_out.txt", "w")
 in_file2 = open("Episode_V.txt", "r")
@@ -20,17 +18,18 @@ episode4_line_list = []
 episode5_line_list = []
 episode6_line_list = []
 temp_list = []
+quote_char = '"'
 
 for line_str in in_file1:
     temp_list = line_str.split(" ", 2)
     episode4_line = ["0", "character", "words"]
     for elem in range(len(temp_list)):
-        episode4_line[elem] = temp_list[elem].translate(str.maketrans('', '', string.punctuation))
+        episode4_line[elem] = temp_list[elem].translate(str.maketrans('', '', quote_char))
         episode4_line[elem] = episode4_line[elem].strip()
         if elem == 2:
             episode4_line[elem] = episode4_line[elem].split()
             for i in range(len(episode4_line[elem])):    
-                if episode4_line[elem][0].isupper() and episode4_line[elem][1].isupper():
+                if episode4_line[elem][0].isupper() and len(episode4_line[elem][0]) > 1:
                     temp_list2 = episode4_line[elem].pop(0)
                     episode4_line[elem-1] += (' ' + temp_list2)
             episode4_line[elem] = tuple(episode4_line[elem])
@@ -44,12 +43,12 @@ for line_str in in_file2:
     temp_list = line_str.split(" ", 2)
     episode5_line = ["0", "character", "words"]
     for elem in range(len(temp_list)):
-        episode5_line[elem] = temp_list[elem].translate(str.maketrans('', '', string.punctuation))
+        episode5_line[elem] = temp_list[elem].translate(str.maketrans('', '', quote_char))
         episode5_line[elem] = episode5_line[elem].strip()
         if elem == 2:
             episode5_line[elem] = episode5_line[elem].split()
             for i in range(len(episode5_line[elem])):    
-                if episode5_line[elem][0].isupper() and episode5_line[elem][1].isupper():
+                if episode5_line[elem][0].isupper() and len(episode5_line[elem][0]) > 1:
                     temp_list2 = episode5_line[elem].pop(0)
                     episode5_line[elem-1] += (' ' + temp_list2)
             episode5_line[elem] = tuple(episode5_line[elem])
@@ -63,12 +62,12 @@ for line_str in in_file3:
     temp_list = line_str.split(" ", 2)
     episode6_line = ["0", "character", "words"]
     for elem in range(len(temp_list)):
-        episode6_line[elem] = temp_list[elem].translate(str.maketrans('', '', string.punctuation))
+        episode6_line[elem] = temp_list[elem].translate(str.maketrans('', '', quote_char))
         episode6_line[elem] = episode6_line[elem].strip()
         if elem == 2:
             episode6_line[elem] = episode6_line[elem].split()
             for i in range(len(episode6_line[elem])):    
-                if episode6_line[elem][0].isupper():
+                if episode6_line[elem][0].isupper() and len(episode6_line[elem][0]) > 1:
                     temp_list2 = episode6_line[elem].pop(0)
                     episode6_line[elem-1] += (' ' + temp_list2)
             episode6_line[elem] = tuple(episode6_line[elem])
